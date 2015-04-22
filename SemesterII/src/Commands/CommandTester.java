91c4ac78b9c8b28d031222665219ba32c2c63078
David Yesil <David.Yesil@hs-augsburg.de>
2015-04-22 10:41:00 +0200

    Arbeit in der Uni 1.

--- SemesterII/src/Commands/CommandTester.java
+++ SemesterII/src/Commands/CommandTester.java
@@ -1,18 +1,21 @@
 package Commands;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class CommandTester {
-	public static void main(String[] args ){
+	public static void main(String[] args )throws IOException {
 		
-		
-		BufferedReader lol = new BufferedReader(new InputStreamReader(System.in));
-		CommandScanner scannerBoi = new CommandScanner(StockGameCommandType.values(),lol);
-		Class<?>[] arr = new Class<?>[4];
-		Class<?>[] arr2 = new Class<?>[]{String.class, String.class , Integer.class};
+		CommandDescriptor commandDescriptor = new CommandDescriptor();
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+		CommandScanner commandScanner = new CommandScanner(StockGameCommandType.values(),bufferedReader);
+//		Class<?>[] arr = new Class<?>[4];
+//		Class<?>[] arr2 = new Class<?>[]{String.class, String.class , Integer.class};
 		
 		StockGameCommandType wtf = StockGameCommandType.BUYSHARE;
 		System.out.println(wtf.getParamTypes().toString());
+		
+		commandScanner.inputLine2CommandDescriptor(commandDescriptor);
 	}
 }
