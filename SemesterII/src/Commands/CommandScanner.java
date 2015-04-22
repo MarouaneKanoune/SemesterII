bf276815367652dce47b6f5eafef622f0a9ffd66
David Yesil <David.Yesil@hs-augsburg.de>
2015-04-22 10:41:00 +0200

    Arbeit in der Uni 1.

--- SemesterII/src/Commands/CommandScanner.java
+++ SemesterII/src/Commands/CommandScanner.java
@@ -1,14 +1,32 @@
 package Commands;
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 
 public class CommandScanner {
 
+	private BufferedReader reader;
 	private ArrayList<CommandTypeInfo> commandTypes = new ArrayList<>();
 	
 	public CommandScanner(CommandTypeInfo[] commandTypeInfo, BufferedReader reader){
-		commandTypes.addAll(Arrays.asList(commandTypeInfo));	//factory method which allows arrays to be viewed as lists.		
+		commandTypes.addAll(Arrays.asList(commandTypeInfo));
+		this.reader = reader;
+	}
+	
+	public void inputLine2CommandDescriptor(CommandDescriptor commandDescriptor)throws IOException{
+		String[] line = this.reader.readLine().split(" ");
+		Iterator<CommandTypeInfo> newIterator = commandTypes.iterator();
+		
+		
+		while(newIterator.hasNext()){
+			CommandTypeInfo commandTypeInfo = newIterator.next();
+			if(commandTypeInfo.getCommand().equals(line[0])){
+				commandDescriptor.setCommandType(commandTypeInfo);
+				break;
+			}
+		}
 	}
 
 	
